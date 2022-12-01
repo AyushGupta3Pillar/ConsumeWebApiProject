93dd1c0d14b34abe9758f477df0d2f748f88ff91
ayush.gupta1 <ayush.gupta1@NDI-LAP-122>
2022-12-01 16:08:26 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ConsumeWebApiProject.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(35, ErrorMessage = "Name not be exceed")]
+        public string name { get; set; }
+        [Required]
+        public string gender { get; set; }
+
+        [Required]
+        [Range(18, 55, ErrorMessage = "Please Enter age between 18 and 55")]
+        public int age { get; set; }
+        [Required]
+        public string designation { get; set; }
+        [Required]
+        public int salary { get; set; }
+    }
+}
